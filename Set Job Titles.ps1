cb9cedad1f7d2bdb1fa9c5a2654c49d07ae695bf
Harleycox <68119890+Harleycox@users.noreply.github.com>
2021-11-19 17:03:09 +0000

    Add files via upload

---
+++ Set Job Titles.ps1
@@ -0,0 +1,7 @@
+﻿$users = Import-Csv -path "C:\Users\absolute.networks\Desktop\staff.csv"
+foreach ($user in $users)
+{
+   $userstring = ($user.Name).ToString()
+   $temp = get-aduser -filter {Name -like $userstring}
+   Set-ADUser $temp -Title $user.'Job Title'
+}
